e925ef439b068b1cc97141d07f2a5d24a4add7e5
Darius Ra <niuzas@gmail.com>
2023-11-17 06:35:39 +0200

    db local false

--- tina/database.ts
+++ tina/database.ts
@@ -7,7 +7,7 @@ import path from 'path'
 import fs from 'fs'
 
 // Manage this flag in your CI/CD pipeline and make sure it is set to false in production
-const isLocal = process.env.TINA_PUBLIC_IS_LOCAL === 'true'
+const isLocal = process.env.TINA_PUBLIC_IS_LOCAL === 'false'
 
 if (isLocal) console.log('Running TinaCMS in local mode.')
 else console.log('Running TinaCMS in production mode.')
